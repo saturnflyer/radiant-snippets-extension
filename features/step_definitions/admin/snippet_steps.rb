85e184e581ec29e4448678648c3461946953d1a0
Jim Gay <jim@saturnflyer.com>
2012-03-02 10:30:32 -0500

    add feature for pagination from core

--- features/step_definitions/admin/snippet_steps.rb
+++ features/step_definitions/admin/snippet_steps.rb
@@ -1,3 +1,21 @@
+Given /^There are many snippets$/ do
+  100.times do |i|
+    Snippet.create(:name => "snippet_#{i}", :content => "This is snippet #{i}")
+  end
+end
+
+Given /^There are few snippets$/ do
+  #
+end
+
+Then /^I should see all the snippets$/ do
+  Snippet.all.each do |snippet|
+    response.body.should have_tag('tr.snippet') do
+      with_tag("a", :text => snippet.name)
+    end
+  end
+end
+
 When /^I view a snippet$/ do
   visit "/admin/snippets/#{snippets(:first).id}"
 end
